fbdbc08d298eef8548a49ceea59afd9c059f0778
Upendra Kumar <upendraget@gmail.com>
2026-04-25 09:47:07 +0530

    Fixed.

--- Product.cs
+++ Product.cs
@@ -3,8 +3,7 @@ namespace ProductAPI
     public class Product
     {
         public int Id { get; set; }
-        public string Name  { get; set; }
-     }
-
+        public string Name { get; set; }
+    }
 
 }
